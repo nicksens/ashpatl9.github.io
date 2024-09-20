cfed383a82290b768d4b37ff4cbc784f5473946c
nicksens <145575172+nicksens@users.noreply.github.com>
2024-09-20 19:19:38 +0700

    Add files via upload

---
+++ events/app.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+
+var video = document.getElementById("myVideo");
+
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
